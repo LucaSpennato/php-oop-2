c74e3fb6bc2526faef66c31a19c5582786e4d07c
LucaSpennato <luca.spennato@gmail.com>
2022-09-06 16:45:46 +0200

    guest creat, printed prices

--- index.php
+++ index.php
@@ -23,9 +23,25 @@
             $user->addToCart(new Food("cibbo", 33.44, 2313132425, "pappabella", "can", true, "dog", "bono", true, "20/04/2500", "400g"));
             $user->addToCart(new Utilities("pillolazze", 50.33, 1415151515, "pillazze per cose", "3x2"));
             var_dump($user);
-            echo $user->getDiscounted();
+
+            $guest = new User();
+            $guest->addToCart(new Food("pappa", 33.55, 1919191919, "pappa altamente nutriente", "busta", false, "cat", "pocobono", true, "20/01/3000", "445g"));
+            $guest->addToCart(new Utilities("Cool Shaders", 5000.22, 1010101010, "coolest shaders ever", "30cmX10cm"));
+            var_dump($guest);
         ?>
     </h3>
+
+    <h5>
+        <?php 
+            echo "Prezzo utente registrato: " . $user->getDiscounted();
+        ?>
+    </h5>
+
+    <h5>
+        <?php
+            echo "Prezzo ospite: " . $guest->getCartTotal();
+        ?>
+    </h5>
 
 </body>
 </html>
